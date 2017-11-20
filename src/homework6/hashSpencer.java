e6974b145237565ef89c18f210146e890547dfb8
Anisha Dubhashi <anisha.dubhashi@gmail.com>
2017-11-20 15:44:53 -0600

    add read file

--- src/homework6/hashSpencer.java
+++ src/homework6/hashSpencer.java
@@ -1,5 +1,30 @@
 package homework6;
 
-public class hashSpencer {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class hashSpencer {
+	//main
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		String pathname = "../input.txt";
+		File file = new File(pathname);
+		
+		String[] names = new String[14];
+		
+		int i = 0; 
+		try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) 
+		{
+			String line = null; // don't skip header
+			while ((line = bufferedReader.readLine()) != null) 
+			{
+				//names[i] = line.split(",")[0]; 
+				names[i] = line;
+				i++;
+			}
+		}
+	}
+		
 }
